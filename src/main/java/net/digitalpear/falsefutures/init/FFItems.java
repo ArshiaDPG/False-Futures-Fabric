56c879ee0fd685d238b590425a0a72f2014393af
DP <digitalpeargaming@gmail.com>
2022-10-26 15:29:17 -0700

    new spawn egg method

--- src/main/java/net/digitalpear/falsefutures/init/FFItems.java
+++ src/main/java/net/digitalpear/falsefutures/init/FFItems.java
@@ -1,6 +1,8 @@
 package net.digitalpear.falsefutures.init;
 
 import net.digitalpear.falsefutures.FalseFutures;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SpawnEggItem;
@@ -11,8 +13,10 @@ public class FFItems {
     public static Item register(String itemID, Item item){
         return Registry.register(Registry.ITEM, new Identifier(FalseFutures.MOD_ID, itemID), item);
     }
-    public static final Item GIPPLE_SPAWN_EGG = register("gipple_spawn_egg", new SpawnEggItem(FFEntities.GIPPLE, 13558777, 11642584,
-            new Item.Settings().group(ItemGroup.MISC)));
+    public static Item createSpawnEgg(EntityType<? extends MobEntity> type, int PrimaryColor, int SecondaryColor){
+        return register(type.getTranslationKey().split("\\.")[2] + "_spawn_egg", new SpawnEggItem(type, PrimaryColor, SecondaryColor, new Item.Settings().group(ItemGroup.MISC)));
+    }
+    public static final Item GIPPLE_SPAWN_EGG = createSpawnEgg(FFEntities.GIPPLE, 13558777, 11642584);
     public static final Item GELATIN = register("gelatin", new Item(new Item.Settings().food(FFFoodComponents.GELATIN).group(ItemGroup.FOOD)));
 
 

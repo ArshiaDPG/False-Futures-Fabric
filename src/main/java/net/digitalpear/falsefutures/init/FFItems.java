ca9175dd1f9d8137265b15603ed1b35c368749db
DP <digitalpeargaming@gmail.com>
2022-11-17 17:22:27 -0800

    Item reordering

--- src/main/java/net/digitalpear/falsefutures/init/FFItems.java
+++ src/main/java/net/digitalpear/falsefutures/init/FFItems.java
@@ -13,6 +13,7 @@ import net.minecraft.util.registry.Registry;
 
 @SuppressWarnings("unused")
 public class FFItems {
+
     public static Item register(String itemID, Item item){
         return Registry.register(Registry.ITEM, new Identifier(FalseFutures.MOD_ID, itemID), item);
     }
@@ -25,14 +26,15 @@ public class FFItems {
     public static Item createBucketedMob(EntityType<?> type){
         return register(type.getTranslationKey().split("\\.")[2] + "_bucket", new EntityBucketItem(type, Fluids.WATER, SoundEvents.ITEM_BUCKET_EMPTY_FISH, (new Item.Settings()).maxCount(1).group(ItemGroup.MISC)));
     }
+
+    public static final Item GIPPLE_BUCKET = createBucketedMob(FFEntities.GIPPLE);
+
     public static final Item GIPPLE_SPAWN_EGG = createSpawnEgg(FFEntities.GIPPLE, 13558777, 11642584);
     public static final Item SOMETHING_SPAWN_EGG = createSpawnEgg(FFEntities.SOMETHING, 13558777, 9669861);
 
     public static final Item GELATIN = register("gelatin", new Item(new Item.Settings().food(FFFoodComponents.GELATIN).group(ItemGroup.FOOD)));
 
     public static final Item MUSIC_DISC_GIPPLECORE = createDisc("gipplecore", 13, FFSoundEvents.MUSIC_DISC_GIPPLECORE, 113);
-
-    public static final Item GIPPLE_BUCKET = createBucketedMob(FFEntities.GIPPLE);
 
     public static final Item GIPPLEPAD = register("gipplepad", new PlaceableOnWaterItem(FFBlocks.GIPPLEPAD, new Item.Settings()));
 

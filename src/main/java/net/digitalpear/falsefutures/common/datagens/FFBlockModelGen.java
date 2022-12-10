31b3d3bce57dd2142514ada7fc7bbc2d5ff1f79f
DP <digitalpeargaming@gmail.com>
2022-12-09 20:52:33 -0800

    item model generation

--- src/main/java/net/digitalpear/falsefutures/common/datagens/FFBlockModelGen.java
+++ src/main/java/net/digitalpear/falsefutures/common/datagens/FFBlockModelGen.java
@@ -1,9 +1,12 @@
 package net.digitalpear.falsefutures.common.datagens;
 
+import net.digitalpear.falsefutures.init.FFItems;
+import net.digitalpear.falsefutures.init.tags.FFItemTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
 
 public class FFBlockModelGen extends FabricModelProvider {
     public FFBlockModelGen(FabricDataGenerator dataGenerator) {
@@ -16,6 +19,9 @@ public class FFBlockModelGen extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-
+        itemModelGenerator.register(FFItems.GELATIN, Models.GENERATED);
+        itemModelGenerator.register(FFItems.GIPPLE_BUCKET, Models.GENERATED);
+        itemModelGenerator.register(FFItems.MUSIC_DISC_GIPPLECORE, Models.GENERATED);
+        itemModelGenerator.register(FFItems.GIPPLEPAD, Models.GENERATED);
     }
 }

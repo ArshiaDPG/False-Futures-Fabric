e3f1c095ed4137c73a286ffb8480ef2eeeb1fe7d
DP <digitalpeargaming@gmail.com>
2022-12-19 17:15:40 -0800

    datagen improvements

--- src/main/java/net/digitalpear/falsefutures/common/datagens/FFRecipeGen.java
+++ src/main/java/net/digitalpear/falsefutures/common/datagens/FFRecipeGen.java
@@ -12,6 +12,8 @@ import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.StonecuttingRecipe;
 import net.minecraft.util.registry.Registry;
 
 import java.util.HashMap;
@@ -38,19 +40,6 @@ public class FFRecipeGen extends FabricRecipeProvider {
     protected void generateRecipes(Consumer<RecipeJsonProvider> exporter) {
         JELLY.forEach((jelly, ingredient) -> offerJellyRecipe(exporter, jelly, ingredient));
 
-
-        ShapedRecipeJsonBuilder.create(FFBlocks.GELASTONE_BRICKS, 4)
-                .input('X', FFBlocks.GELASTONE)
-                .pattern("XX")
-                .pattern("XX")
-                .criterion("has_gelastone", conditionsFromItem(FFBlocks.GELASTONE)).offerTo(exporter);
-
-        ShapedRecipeJsonBuilder.create(FFBlocks.DEEP_GELASTONE_BRICKS, 4)
-                .input('X', FFBlocks.DEEP_GELASTONE)
-                .pattern("XX")
-                .pattern("XX")
-                .criterion("has_deep_gelastone", conditionsFromItem(FFBlocks.DEEP_GELASTONE)).offerTo(exporter);
-
         ShapelessRecipeJsonBuilder.create(FFBlocks.GELASTONE)
                 .input(Blocks.COBBLESTONE)
                 .input(FFItems.GELATIN)
@@ -58,9 +47,23 @@ public class FFRecipeGen extends FabricRecipeProvider {
         ShapelessRecipeJsonBuilder.create(FFBlocks.DEEP_GELASTONE)
                 .input(Blocks.COBBLED_DEEPSLATE)
                 .input(FFItems.GELATIN)
-                .criterion("has_gelatin", conditionsFromItem(FFItems.GELATIN)).offerTo(exporter);
+                .criterion("has_deep_gelatin", conditionsFromItem(FFItems.GELATIN)).offerTo(exporter);
 
 
+
+        ShapedRecipeJsonBuilder.create(FFBlocks.GELASTONE_BRICKS, 4)
+                .input('X', FFBlocks.GELASTONE)
+                .pattern("XX")
+                .pattern("XX")
+                .criterion("has_gelastone", conditionsFromItem(FFBlocks.GELASTONE)).offerTo(exporter);
+        FabricRecipeProvider.offerStonecuttingRecipe(exporter, FFBlocks.GELASTONE, FFBlocks.GELASTONE_BRICKS);
+
+        ShapedRecipeJsonBuilder.create(FFBlocks.DEEP_GELASTONE_BRICKS, 4)
+                .input('X', FFBlocks.DEEP_GELASTONE)
+                .pattern("XX")
+                .pattern("XX")
+                .criterion("has_deep_gelastone", conditionsFromItem(FFBlocks.DEEP_GELASTONE)).offerTo(exporter);
+        FabricRecipeProvider.offerStonecuttingRecipe(exporter, FFBlocks.DEEP_GELASTONE, FFBlocks.DEEP_GELASTONE_BRICKS);
 
         makeStoneRecipes(exporter, FFBlocks.GELASTONE, FFBlocks.GELASTONE_STAIRS, FFBlocks.GELASTONE_SLAB, FFBlocks.GELASTONE_BUTTON,
                 FFBlocks.GELASTONE_PRESSURE_PLATE, FFBlocks.GELASTONE_WALL);
@@ -90,12 +93,11 @@ public class FFRecipeGen extends FabricRecipeProvider {
                 .pattern("CC ")
                 .pattern("CCC")
                 .criterion(criterion, conditionsFromItem(input)).offerTo(exporter);
+        FabricRecipeProvider.offerStonecuttingRecipe(exporter, input, output);
     }
     public static void makeSlab(Consumer<RecipeJsonProvider> exporter, Block input, Block output, String criterion){
-        ShapedRecipeJsonBuilder.create(output, 6)
-                .input('C', input)
-                .pattern("CCC")
-                .criterion(criterion, conditionsFromItem(input)).offerTo(exporter);
+        FabricRecipeProvider.createStairsRecipe(output, Ingredient.ofItems(input)).criterion(criterion, conditionsFromItem(input)).offerTo(exporter);
+        FabricRecipeProvider.offerStonecuttingRecipe(exporter, input, output, 2);
     }
     public static void makeWall(Consumer<RecipeJsonProvider> exporter, Block input, Block output, String criterion){
         ShapedRecipeJsonBuilder.create(output, 6)
@@ -103,17 +105,20 @@ public class FFRecipeGen extends FabricRecipeProvider {
                 .pattern("CCC")
                 .pattern("CCC")
                 .criterion(criterion, conditionsFromItem(input)).offerTo(exporter);
+        FabricRecipeProvider.offerStonecuttingRecipe(exporter, input, output);
     }
     public static void makeButton(Consumer<RecipeJsonProvider> exporter, Block input, Block output, String criterion){
         ShapelessRecipeJsonBuilder.create(output)
                 .input(input)
                 .criterion(criterion, conditionsFromItem(input)).offerTo(exporter);
+        FabricRecipeProvider.offerStonecuttingRecipe(exporter, input, output);
     }
     public static void makePressurePlate(Consumer<RecipeJsonProvider> exporter, Block input, Block output, String criterion){
         ShapedRecipeJsonBuilder.create(output)
                 .input('C', input)
                 .pattern("CC")
                 .criterion(criterion, conditionsFromItem(input)).offerTo(exporter);
+        FabricRecipeProvider.offerStonecuttingRecipe(exporter, input, output);
     }
 
     public static void makeStoneRecipes(Consumer<RecipeJsonProvider> exporter, Block input, Block stairs, Block slab, Block button, Block pressurePlate, Block wall){

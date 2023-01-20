d7bf9f5216c46c188295fae411f4985fc64e7160
DP <digitalpeargaming@gmail.com>
2023-01-19 19:34:58 -0800

    gipple girl will no longer try to summon the dark lord

--- src/main/java/net/digitalpear/falsefutures/FalseFuturesConfig.java
+++ src/main/java/net/digitalpear/falsefutures/FalseFuturesConfig.java
@@ -5,7 +5,6 @@ import com.kyanite.paragon.api.interfaces.configtypes.JSONModConfig;
 
 public class FalseFuturesConfig implements JSONModConfig {
     public static final ConfigOption<Boolean> CAN_PET_GIPPLE = new ConfigOption<>("can_pet_gipples", "Can players pet gipples by right clicking them?", true);
-    public static final ConfigOption<Integer> GIPPLE_SPAWNING_WEIGHT = new ConfigOption<>("gipple_spawning_weight", "Spawning frequency of the gipple.", 2);
     public static final ConfigOption<Integer> SOMETHING_SPAWNING_PERCENTAGE = new ConfigOption<>("something_spawning_percentage", "Set to 0 to disable.", 10);
 
     public static final ConfigOption<Boolean> JELLY_SPECIAL_EFFECTS = new ConfigOption<>("jelly_special_effects", "Will jelly give special effects upon being eaten?", true);

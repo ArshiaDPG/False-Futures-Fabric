f845fd0a5cf60031a1275e18dff04fd9e73e76a8
DP <digitalpeargaming@gmail.com>
2023-04-02 11:19:09 -0700

    blocks now drop happily

--- src/main/java/net/digitalpear/falsefutures/common/datagens/FFBlockLootTableGen.java
+++ src/main/java/net/digitalpear/falsefutures/common/datagens/FFBlockLootTableGen.java
@@ -1,17 +1,16 @@
 package net.digitalpear.falsefutures.common.datagens;
 
-import net.digitalpear.falsefutures.FalseFutures;
 import net.digitalpear.falsefutures.common.blocks.GippleInfestedBlock;
+import net.digitalpear.falsefutures.common.blocks.TallJellyrootBlock;
 import net.digitalpear.falsefutures.init.FFBlocks;
 import net.digitalpear.falsefutures.init.FFItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.minecraft.block.Block;
 import net.minecraft.block.TallPlantBlock;
 import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.data.server.BlockLootTableGenerator;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
@@ -19,79 +18,72 @@ import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
 import net.minecraft.loot.condition.LocationCheckLootCondition;
 import net.minecraft.loot.condition.MatchToolLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
-import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LeafEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.ApplyBonusLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
-import net.minecraft.util.Identifier;
+import net.minecraft.predicate.BlockPredicate;
+import net.minecraft.predicate.StatePredicate;
+import net.minecraft.predicate.entity.LocationPredicate;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
 
-import java.util.List;
-import java.util.function.BiConsumer;
+import java.util.Arrays;
 
-public class FFBlockLootTableGen extends SimpleFabricLootTableProvider {
+public class FFBlockLootTableGen extends FabricBlockLootTableProvider {
     private static final net.minecraft.loot.condition.LootCondition.Builder WITH_SHEARS = MatchToolLootCondition.builder(net.minecraft.predicate.item.ItemPredicate.Builder.create().items(Items.SHEARS));
+
     public FFBlockLootTableGen(FabricDataGenerator dataGenerator) {
-        super(dataGenerator, LootContextTypes.BLOCK);
+        super(dataGenerator);
     }
 
     @Override
-    public void accept(BiConsumer<Identifier, LootTable.Builder> biConsumer) {
-        simpleDrop(biConsumer, FFBlocks.GIPPLEPAD);
+    protected void generateBlockLootTables() {
+        stoneSet(FFBlocks.BRINESHALE,
+                FFBlocks.BRINESHALE_STAIRS,
+                FFBlocks.BRINESHALE_SLAB,
+                FFBlocks.BRINESHALE_WALL,
+                FFBlocks.BRINESHALE_BUTTON,
+                FFBlocks.BRINESHALE_PRESSURE_PLATE);
+        drops(FFBlocks.CHISELED_BRINESHALE_BRICKS);
 
-        stoneSet(biConsumer, FFBlocks.GELATITE, FFBlocks.GELATITE_STAIRS, FFBlocks.GELATITE_SLAB, FFBlocks.GELATITE_WALL, FFBlocks.GELATITE_BUTTON,
+        stoneSet(FFBlocks.GELATITE,
+                FFBlocks.GELATITE_STAIRS,
+                FFBlocks.GELATITE_SLAB,
+                FFBlocks.GELATITE_WALL,
+                FFBlocks.GELATITE_BUTTON,
                 FFBlocks.GELATITE_PRESSURE_PLATE);
-        simpleDrop(biConsumer, FFBlocks.CHISELED_GELATITE_BRICKS);
-        stoneSet(biConsumer, FFBlocks.GELATITE_BRICKS, FFBlocks.GELATITE_BRICK_STAIRS, FFBlocks.GELATITE_BRICK_SLAB, FFBlocks.GELATITE_BRICK_WALL);
-        stoneSet(biConsumer, FFBlocks.BRINESHALE, FFBlocks.BRINESHALE_STAIRS, FFBlocks.BRINESHALE_SLAB, FFBlocks.BRINESHALE_WALL,
-                FFBlocks.BRINESHALE_BUTTON, FFBlocks.BRINESHALE_PRESSURE_PLATE);
-        simpleDrop(biConsumer, FFBlocks.CHISELED_BRINESHALE_BRICKS);
-        stoneSet(biConsumer, FFBlocks.BRINESHALE_BRICKS, FFBlocks.BRINESHALE_BRICK_STAIRS, FFBlocks.BRINESHALE_BRICK_SLAB,
-                FFBlocks.BRINESHALE_BRICK_WALL);
 
-        biConsumer.accept(new Identifier(FalseFutures.MOD_ID, "blocks/" + Registry.BLOCK.getId(FFBlocks.JELLYROOT).getPath()), jellyrootDrops(FFBlocks.JELLYROOT));
-        biConsumer.accept(new Identifier(FalseFutures.MOD_ID, "blocks/" + Registry.BLOCK.getId(FFBlocks.TALL_JELLYROOT).getPath()), tallJellyrootDrops(FFBlocks.TALL_JELLYROOT, FFBlocks.JELLYROOT));
-        biConsumer.accept(new Identifier(FalseFutures.MOD_ID, "blocks/" + Registry.BLOCK.getId(FFBlocks.POTTED_JELLYROOT).getPath()), BlockLootTableGenerator.pottedPlantDrops(FFBlocks.JELLYROOT));
+        stoneSet(FFBlocks.GELATITE_BRICKS, FFBlocks.GELATITE_BRICK_STAIRS, FFBlocks.GELATITE_BRICK_SLAB, FFBlocks.GELATITE_BRICK_WALL);
+        stoneSet(FFBlocks.BRINESHALE_BRICKS, FFBlocks.BRINESHALE_BRICK_STAIRS, FFBlocks.BRINESHALE_BRICK_SLAB, FFBlocks.BRINESHALE_BRICK_WALL);
 
-        GippleInfestedBlock.REGULAR_TO_INFESTED_BLOCK.forEach((regular, infested) -> {
-            biConsumer.accept(new Identifier(FalseFutures.MOD_ID, "blocks/" + Registry.BLOCK.getId(infested).getPath()), BlockLootTableGenerator.dropsWithSilkTouch(regular));
+        GippleInfestedBlock.REGULAR_TO_INFESTED_BLOCK.forEach((regular, infested) -> drops(infested));
 
-        });
+
+        tallJellyrootDrops(FFBlocks.TALL_JELLYROOT, FFBlocks.JELLYROOT);
     }
+
     public static LootTable.Builder jellyrootDrops(Block dropWithShears) {
-        return BlockLootTableGenerator.dropsWithShears(dropWithShears, (net.minecraft.loot.entry.LootPoolEntry.Builder)BlockLootTableGenerator.applyExplosionDecay(dropWithShears, ((net.minecraft.loot.entry.LeafEntry.Builder)ItemEntry.builder(FFItems.GELATIN).conditionally(RandomChanceLootCondition.builder(0.125F))).apply(ApplyBonusLootFunction.uniformBonusCount(Enchantments.FORTUNE, 2))));
+        return dropsWithShears(dropWithShears, (LootPoolEntry.Builder)applyExplosionDecay(dropWithShears, ((LeafEntry.Builder) ItemEntry.builder(FFItems.GELATIN).conditionally(RandomChanceLootCondition.builder(0.125F))).apply(ApplyBonusLootFunction.uniformBonusCount(Enchantments.FORTUNE, 2))));
     }
-    public static LootTable.Builder tallJellyrootDrops(Block tallRoot, Block root) {
-        net.minecraft.loot.entry.LootPoolEntry.Builder<?> builder = ItemEntry.builder(root).apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(2.0F))).conditionally(WITH_SHEARS).alternatively(((net.minecraft.loot.entry.LeafEntry.Builder)BlockLootTableGenerator.addSurvivesExplosionCondition(tallRoot, ItemEntry.builder(FFItems.GELATIN))).conditionally(RandomChanceLootCondition.builder(0.125F)));
-        return LootTable.builder().pool(LootPool.builder().with(builder).conditionally(BlockStatePropertyLootCondition.builder(tallRoot).properties(net.minecraft.predicate.StatePredicate.Builder.create().exactMatch(TallPlantBlock.HALF, DoubleBlockHalf.LOWER))).conditionally(LocationCheckLootCondition.builder(net.minecraft.predicate.entity.LocationPredicate.Builder.create().block(net.minecraft.predicate.BlockPredicate.Builder.create().blocks(new Block[]{tallRoot}).state(net.minecraft.predicate.StatePredicate.Builder.create().exactMatch(TallPlantBlock.HALF, DoubleBlockHalf.UPPER).build()).build()), new BlockPos(0, 1, 0)))).pool(LootPool.builder().with(builder).conditionally(BlockStatePropertyLootCondition.builder(tallRoot).properties(net.minecraft.predicate.StatePredicate.Builder.create().exactMatch(TallPlantBlock.HALF, DoubleBlockHalf.UPPER))).conditionally(LocationCheckLootCondition.builder(net.minecraft.predicate.entity.LocationPredicate.Builder.create().block(net.minecraft.predicate.BlockPredicate.Builder.create().blocks(new Block[]{tallRoot}).state(net.minecraft.predicate.StatePredicate.Builder.create().exactMatch(TallPlantBlock.HALF, DoubleBlockHalf.LOWER).build()).build()), new BlockPos(0, -1, 0))));
-    }
-
-    public static void simpleDrop(BiConsumer<Identifier, LootTable.Builder> biConsumer, Block block){
-        biConsumer.accept(new Identifier(FalseFutures.MOD_ID, "blocks/" + Registry.BLOCK.getId(block).getPath()), BlockLootTableGenerator.drops(block));
-    }
-    public static void infestedBlockDrop(BiConsumer<Identifier, LootTable.Builder> biConsumer, Block block, Block drop){
-        biConsumer.accept(new Identifier(FalseFutures.MOD_ID, "blocks/" + Registry.BLOCK.getId(block).getPath()), BlockLootTableGenerator.dropsWithSilkTouch(drop));
+    public static LootTable.Builder tallJellyrootDrops(Block tallGrass, Block grass) {
+        LootPoolEntry.Builder<?> builder = ItemEntry.builder(grass).apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(2.0F))).conditionally(WITH_SHEARS).alternatively(((LeafEntry.Builder)addSurvivesExplosionCondition(tallGrass, ItemEntry.builder(FFItems.GELATIN))).conditionally(RandomChanceLootCondition.builder(0.125F)));
+        return LootTable.builder().pool(LootPool.builder().with(builder).conditionally(BlockStatePropertyLootCondition.builder(tallGrass).properties(StatePredicate.Builder.create().exactMatch(TallJellyrootBlock.HALF, DoubleBlockHalf.LOWER))).conditionally(LocationCheckLootCondition.builder(LocationPredicate.Builder.create().block(BlockPredicate.Builder.create().blocks(tallGrass).state(StatePredicate.Builder.create().exactMatch(TallJellyrootBlock.HALF, DoubleBlockHalf.UPPER).build()).build()), new BlockPos(0, 1, 0)))).pool(LootPool.builder().with(builder).conditionally(BlockStatePropertyLootCondition.builder(tallGrass).properties(StatePredicate.Builder.create().exactMatch(TallJellyrootBlock.HALF, DoubleBlockHalf.UPPER))).conditionally(LocationCheckLootCondition.builder(LocationPredicate.Builder.create().block(BlockPredicate.Builder.create().blocks(tallGrass).state(StatePredicate.Builder.create().exactMatch(TallJellyrootBlock.HALF, DoubleBlockHalf.LOWER).build()).build()), new BlockPos(0, -1, 0))));
     }
 
-    public static void stoneSet(BiConsumer<Identifier, LootTable.Builder> biConsumer, Block stone, Block stairs, Block slab, Block wall, Block button, Block pressurePlate){
-        simpleDrop(biConsumer, stone);
-        simpleDrop(biConsumer, stairs);
-        biConsumer.accept(new Identifier(FalseFutures.MOD_ID, "blocks/" + Registry.BLOCK.getId(slab).getPath()), BlockLootTableGenerator.slabDrops(slab));
-        simpleDrop(biConsumer, wall);
-        simpleDrop(biConsumer, button);
-        simpleDrop(biConsumer, pressurePlate);
+    public void stoneSet(Block stone, Block stairs, Block slab, Block wall, Block button, Block pressurePlate){
+        singleDropBlocks(stone, stairs, wall, button, pressurePlate);
+        addDrop(slab, BlockLootTableGenerator::slabDrops);
     }
-    public static void stoneSet(BiConsumer<Identifier, LootTable.Builder> biConsumer, Block stone, Block stairs, Block slab, Block wall){
-        simpleDrop(biConsumer, stone);
-        simpleDrop(biConsumer, stairs);
-        biConsumer.accept(new Identifier(FalseFutures.MOD_ID, "blocks/" + Registry.BLOCK.getId(slab).getPath()), BlockLootTableGenerator.slabDrops(slab));
-        simpleDrop(biConsumer, wall);
+
+    public void stoneSet(Block stone, Block stairs, Block slab, Block wall){
+        addDrop(stone);
+        addDrop(stairs);
+        addDrop(slab, BlockLootTableGenerator::slabDrops);
+        addDrop(wall);
     }
-    public static void singleDropBlocks(BiConsumer<Identifier, LootTable.Builder> biConsumer, Block... stone){
-        List.of(stone).forEach(block -> {
-            simpleDrop(biConsumer, block);
-        });
+    public void singleDropBlocks(Block... stone){
+        Arrays.stream(stone).iterator().forEachRemaining(this::addDrop);
     }
 }
